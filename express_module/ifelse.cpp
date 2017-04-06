67eb9c466692215778c98674330585e106e7043e
anuragjai <14mcmc08@gmail.com>
2017-04-06 13:31:31 +0530

    fifth commit

---
+++ express_module/ifelse.cpp
@@ -0,0 +1,31 @@
+/*  Example Program For If Statement In C++
+    little drops @ thiyagaraaj.com
+
+    Coded By:THIYAGARAAJ MP             */
+
+
+#include<iostream>
+
+
+using namespace std;
+int main()
+{
+     // Variable Declaration
+     int a;
+     while(1)
+     {
+     //Get Input Value
+     cout<<"Enter the Number :";
+     cin>>a;
+     
+     //If Condition Check
+     if(a > 10)
+     {
+        // Block For Condition Success
+        cout<<a<<" Is Greater than 10\n";
+     }
+   }  
+     // Wait For Output Screen
+    
+     return 0;
+ }
